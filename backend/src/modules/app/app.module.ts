30fce3c12c28bfd84b701267cac55a1f8394677e
Jakub Bebjak <bebjakub@gmail.com>
2023-11-30 16:31:58 +0100

    move up queues module

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -3,7 +3,7 @@ import { OrmModule } from '@/modules/storage/orm/orm.module';
 import { AppConfigModule } from '@/modules/appConfig/appConfig.module';
 import { ApiModule } from '@/modules/api/api.module';
 import { CacheManagerModule } from '@/modules/cache-manager/cache-manager.module';
-import { QueuesModule } from '@/modules/currency/queues/queues.module';
+import { QueuesModule } from '@/modules/queues/queues.module';
 
 @Module({
   imports: [
